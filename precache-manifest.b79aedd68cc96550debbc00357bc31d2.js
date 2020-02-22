8cd5e5eceb03b48964c7771a1c916fd439938831
Aaron Kaplowitz <akaplowitz@umass.edu>
2020-02-22 10:44:48 -0500

    Updates

---
+++ precache-manifest.b79aedd68cc96550debbc00357bc31d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7f0519306ffa0638d077c4499ccfcee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0267d076fa43719a824a",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a66edbcde9cc9efd89f0",
+    "url": "/static/js/2.8ef8390b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.8ef8390b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0267d076fa43719a824a",
+    "url": "/static/js/main.d5729492.chunk.js"
+  },
+  {
+    "revision": "c46f15f314e8853b755e",
+    "url": "/static/js/runtime-main.97bd41a0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
