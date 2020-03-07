2465e1cb52c91e15af0bf0c626ae8c7c4c2ce265
Aaron Kaplowitz <akaplowitz@umass.edu>
2020-03-07 18:10:02 -0500

    Updates

---
+++ precache-manifest.ae6f7ffc398c7a2c64506f00a07d95a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b1ab5f138caab53bcdc72ea2d8b50ec",
+    "url": "/fly-with-aaron/index.html"
+  },
+  {
+    "revision": "86424fb78f28dcfe809c",
+    "url": "/fly-with-aaron/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "81eacb53c19c8ee31485",
+    "url": "/fly-with-aaron/static/js/2.9e07fb35.chunk.js"
+  },
+  {
+    "revision": "f23ac18eb49b03518af14108d8a72069",
+    "url": "/fly-with-aaron/static/js/2.9e07fb35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86424fb78f28dcfe809c",
+    "url": "/fly-with-aaron/static/js/main.7b0c001d.chunk.js"
+  },
+  {
+    "revision": "f2447832a575e93ad90f",
+    "url": "/fly-with-aaron/static/js/runtime-main.e5afc462.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b79aedd68cc96550debbc00357bc31d2.js"
+  "/fly-with-aaron/precache-manifest.ae6f7ffc398c7a2c64506f00a07d95a9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fly-with-aaron/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
